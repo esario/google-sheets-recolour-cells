65c551c61134d755d6329401db417d9413690dff
Josh Livni <jlivni@google.com>
2015-10-19 11:31:50 -0700

    add contributing; js lint

--- background_colors.js
+++ background_colors.js
@@ -1,19 +1,29 @@
+/**
+ * @license
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Recolor cells based on when it was last edited.
+ * @author jlivni@google.com (Josh Livni).
+ */
+
 /*
-Copyright 2015 Google Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-
-This script uses the Note (comment) of a cell to store a timestamp.
-Notes are updated on each edit, and background colors are changed when the
-sheet is opened.
+ * This script uses the Note (comment) of a cell to store a timestamp.
+ * Notes are updated on each edit, and background colors are changed when the
+ * sheet is opened.
 */
 
 // Each time a cell is edited, update the comment field with the timestamp,
